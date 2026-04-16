1b26ee28a676dab23203a18516fc932e0616ed16
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2026-04-16 16:56:14 +0700

    Feat : done BE

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import { uploadImageFromBuffer } from "../middlewares/upload.middleware.js";
 import User from "../models/user.model.js";
 
 // Get profile
@@ -36,6 +37,47 @@ export const searchUserByUsername = async (req, res) => {
     console.error("Lỗi khi tìm kiếm người dùng", error);
     return res.status(500).json({
       message: "Lỗi hệ thống  !",
+    });
+  }
+};
+
+// uploadAvatar
+export const uploadAvatar = async (req, res) => {
+  try {
+    const file = req.file;
+    const userId = req.user._id;
+
+    if (!file) {
+      return res.status(400).json({
+        message: "No file upload",
+      });
+    }
+
+    const result = await uploadImageFromBuffer(file.buffer);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        avatarURL: result.secure_url,
+        avatarId: result.public_id,
+      },
+      {
+        new: true,
+      },
+    ).select("avatarUrl");
+
+    if (!updatedUser.avatarURL) {
+      return res.status(400).json({
+        message: "Avatar null",
+      });
+    }
+    return res.status(200).json({
+      avatarURL: updatedUser.avatarURL,
+    });
+  } catch (error) {
+    console.error("Lỗi", error);
+    return res.status(500).json({
+      message: "Failed",
     });
   }
 };
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   getProfile,
   searchUserByUsername,
+  uploadAvatar,
 } from "../controllers/user.controller.js";
+
+import { upload } from "../middlewares/upload.middleware.js";
 // User routes
 const router = express.Router();
 
 router.get("/profile", getProfile);
 router.get("/search", searchUserByUsername);
+router.post("/uploadAvatar", upload.single("file", uploadAvatar));
 
 export default router;
