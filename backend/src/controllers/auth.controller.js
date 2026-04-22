d64035025118d3d6c690ad09eb0adaa92be33c7b
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2026-04-22 20:59:14 +0700

    up

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import Session from "../models/session.model.js";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
 const ACCESS_TOKEN_TTL = "15m";
-const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 1000;
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000;
 // User registration
 export const signUp = async (req, res) => {
   try {
